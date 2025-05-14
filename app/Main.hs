21465d13f624b5cf5f087b907b3e3c5d5a037fdd
He Zhenxing <hezhenxing@gmail.com>
2025-05-14 21:02:28 +0800

    add options and first hello sub command

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import Lib (hello)
+import RIO
+import RIO.Directory
+import RIO.Process
+import Options.Applicative.Simple
+import Lib
+import Paths_nux (version)
 
 main :: IO ()
-main = hello
+main = do
+  (options, cmd) <- simpleOptions
+    $(simpleVersion version)
+    "Nux"
+    "A simple Haskell application"
+    (Options
+      <$> switch ( long "verbose"
+                  <> short 'v'
+                  <> help "Verbose output?"
+                 )
+    )
+    (do addCommand
+          "hello"
+          "Hello, Nux!"
+          (const hello)
+          (pure ())
+    )
+  lo <- logOptionsHandle stderr (optVerbose options)
+  pc <- mkDefaultProcessContext
+  withLogFunc lo $ \lf ->
+    let app = App
+              { appLogFunc = lf
+              , appProcessContext = pc
+              , appOptions = options
+              }
+    in runRIO app $ cmd
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,35 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( hello
   , plus
+  , Options(..)
+  , App(..)
   ) where
 
-hello :: IO ()
-hello = putStrLn "Hello, Nux!"
+import RIO
+import RIO.Process
+
+
+hello :: RIO App ()
+hello = logInfo "Hello, Nux!"
 
 plus :: Int -> Int -> Int
 plus a b = a + b
+
+data Options = Options
+  { optVerbose :: Bool
+  } deriving (Show, Eq)
+
+data App = App
+  { appLogFunc :: !LogFunc
+  , appProcessContext :: !ProcessContext
+  , appOptions :: !Options
+  }
+
+instance HasLogFunc App where
+  logFuncL = lens appLogFunc (\x y -> x { appLogFunc = y })
+instance HasProcessContext App where
+  processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
